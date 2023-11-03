87ee2fdc6da258006bd841941f483bf5352e40a7
Eugene <assassineugene@gmail.com>
2023-11-04 02:17:22 +0600

    Random Waifu website v0.1 ready

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 document.querySelector('button').addEventListener('click', getWaifu)
+document.getElementById('lightSide').addEventListener('click', lightOptionsAppear)
+document.getElementById('darkSide').addEventListener('click', darkOptionsAppear)
+
+function lightOptionsAppear () {
+  document.getElementById('light').style.display = 'flex';
+  document.getElementById('dark').style.display = 'none';
+}
+function darkOptionsAppear () {
+  document.getElementById('dark').style.display = 'flex';
+  document.getElementById('light').style.display = 'none';
+}
+
 
 function getWaifu(){
+    let category = document.querySelector('input[name="categories"]:checked').value;
+    let genre = document.getElementsByName('genre')[0].value;
+    let genre2 = document.getElementsByName('genre')[1].value;
 
-    
-    fetch('https://api.waifu.pics/trap/nsfw/trap')
+    let res = genre !== undefined ? genre : genre2;
+
+    fetch(`https://api.waifu.pics/${category}/${res}`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
           console.log(data)
+          document.querySelector('img').src = data.url
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
-  }
+}
